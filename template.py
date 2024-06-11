45b23aeed7f9afdbd92cebc0fb761a4ed4a0872c
Priyanka Ganesan <gpriyankapms@gmail.com>
2024-06-11 11:55:19 -0500

    requirements added

--- template.py
+++ template.py
@@ -1,8 +1,8 @@
-import os 
+import os
 from pathlib import Path
 import logging
 
-#logging string 
+#logging string
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
 project_name = 'cnnClassifier'
@@ -14,7 +14,7 @@ list_of_files = [
     f"src/{project_name}/utils/__init__.py",
     f"src/{project_name}/config/__init__.py",
     f"src/{project_name}/config/configuration.py",
-    f"src/{project_name}/pipeline.__init__.py",
+    f"src/{project_name}/pipeline/__init__.py",
     f"src/{project_name}/entity/__init__.py",
     f"src/{project_name}/constants/__init__.py",
     "config/config.yaml",
@@ -23,21 +23,26 @@ list_of_files = [
     "requirements.txt",
     "setup.py",
     "research/trials.ipynb",
-    "templates/index.html",
-    
+    "templates/index.html"
+
 
 ]
+
 
 for filepath in list_of_files:
     filepath = Path(filepath)
     filedir, filename = os.path.split(filepath)
 
-    if filedir != "":
-        os.makedirs(filedir,exist_ok=True)
-        logging.info(f"creating directory; {filedir} for the file: {filename}")
+
+    if filedir !="":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory; {filedir} for the file: {filename}")
+
     if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
-        with open(filepath,"w") as f:
+        with open(filepath, "w") as f:
             pass
             logging.info(f"Creating empty file: {filepath}")
+
+
     else:
         logging.info(f"{filename} is already exists")
